426306757466b13088264af2b441857cddcb5ae3
Tim Krambeck <krambeck.t@gmail.com>
2023-11-19 18:03:45 +0100

    added student, controller and service

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,16 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@GetMapping
-	public String hello() {
-		return "Hello World!";
 	}
 
 }
