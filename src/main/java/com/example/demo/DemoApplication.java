9e614ce845e331a240fbfad1922f9d05c54d24d9
Tim Krambeck <krambeck.t@gmail.com>
2023-11-22 15:14:21 +0100

    added hello world to home page

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,10 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "Hello World.";
 	}
 
 }
