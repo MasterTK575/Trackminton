57a4043542cca761e3e11523b14b8629322adfc0
Nicolas Wenzel <nicolas.l.wenzel@outlook.de>
2024-01-16 14:02:46 +0100

    pplayers now saved in db

--- src/main/resources/frontend/angular-app/src/app/start-game/start-game.component.ts
+++ src/main/resources/frontend/angular-app/src/app/start-game/start-game.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataSharingService } from '../services/dataSharingService';
+import { PlayerService } from '../player.service';
 
 @Component({
   selector: 'app-start-game',
@@ -15,7 +16,8 @@ export class StartGameComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private dataSharingService: DataSharingService
+    private dataSharingService: DataSharingService,
+    private playerService: PlayerService
   ) {}
 
   ngOnInit(): void {
@@ -30,6 +32,17 @@ export class StartGameComponent implements OnInit {
       this.team2Player1 &&
       this.team2Player2
     ) {
+      const user1 = this.extractUserData(this.team1Player1);
+      const user2 = this.extractUserData(this.team1Player2);
+      const user3 = this.extractUserData(this.team2Player1);
+      const user4 = this.extractUserData(this.team2Player2);
+
+      // Use PlayerService to add players to the backend
+      this.playerService.addPlayer(user1).subscribe();
+      this.playerService.addPlayer(user2).subscribe();
+      this.playerService.addPlayer(user3).subscribe();
+      this.playerService.addPlayer(user4).subscribe();
+
       const teamData = {
         team1Player1: this.team1Player1,
         team1Player2: this.team1Player2,
@@ -44,5 +57,25 @@ export class StartGameComponent implements OnInit {
       // Redirect to playfield component
       this.router.navigate(['/playfield']);
     }
+  }
+  extractUserData(fullName: string): {
+    firstName: string;
+    lastName: string;
+    userName: string;
+  } {
+    const spaceIndex = fullName.indexOf(' ');
+    let firstName, lastName, userName;
+
+    if (spaceIndex !== -1) {
+      firstName = fullName.substring(0, spaceIndex);
+      lastName = fullName.substring(spaceIndex + 1);
+    } else {
+      firstName = fullName;
+      lastName = '';
+    }
+
+    userName = `${firstName.charAt(0)}${lastName.charAt(0)}`;
+
+    return { firstName, lastName, userName };
   }
 }
